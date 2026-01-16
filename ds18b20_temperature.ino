cefce40693a112a57931dc53cc7a2cef82d03908
karthikm5267-kar <karthikm5267@gmail.com>
2026-01-16 11:45:52 +0530

    Update ds18b20_temperature.ino

--- ds18b20_temperature.ino
+++ ds18b20_temperature.ino
@@ -1,1 +1,28 @@
+// Task-1: Temperature Monitoring using DS18B20
+// CodTech Embedded Systems Internship
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2   // DS18B20 data pin connected to D2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  Serial.println("Temperature Monitoring System Started");
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" °C");
+
+  delay(1000);
+}
 
